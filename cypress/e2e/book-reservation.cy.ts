faa8ce0084db60472327018f42574490a15c9296
Jonathan Azoff <jon@postmodern.llc>
2023-03-04 03:59:34 +0100

    mvp working

--- cypress/e2e/book-reservation.cy.ts
+++ cypress/e2e/book-reservation.cy.ts
@@ -1,112 +1,147 @@
 /// <reference types="cypress" />
 
-interface Target {
-	reservation: string,
+interface Reservation {
+	slug: string,
+	offering: string,
 	partySize: number,
 	timePreferences: string[]
 }
 
-const targetId = Cypress.env('target')
-const target: Target = require(`../fixtures/${targetId}.json`)
-const patron = require('../fixtures/patron.json')
-
-const tid = (id:string, eq:string = '=') => `[data-testid${eq}${id}]`
-
-function addGuestsUntilEqualTo(partySize:number) {
-	return new Cypress.Promise((resolve, reject) => {
-		cy.get(tid('experience-dialog-content'))
-			.get(tid('guest-selector-text') + ' p')
-			.last().then((textNode) => {
-				const numberOfGuests = parseInt(textNode.text(), 10)
-				if (numberOfGuests === target.partySize) {
-					resolve(textNode);
-					return
-				}
-				const slector = numberOfGuests > target.partySize 
-					? 'guest-selector_minus'
-					: 'guest-selector_plus'
-					;
-				cy.get(tid(slector)).last().click().then(() => {
-					// recurse until party size is reached (or fails)
-					addGuestsUntilEqualTo(partySize).then(resolve)
-				})
-		})
-	})
+interface Patron {
+	email: string,
+	password: string,
+	cvv: number
 }
 
-function closeTrusteModal() {
-	return new Cypress.Promise((resolve, reject) => {
-		cy.get('body').then((body) => {
-			const rejectAllCookies = body.find('#truste-consent-required')
-			if (rejectAllCookies.length > 0) 
-				cy.wrap(rejectAllCookies).click().then(resolve)
-			else 
-				resolve(body) 
-		})
-	})
-}
-
-function openBookingModal() {
-	cy.get(tid('offering-link', '*='))
-			.contains(target.reservation)
-			.click()
-}
-
-function fetchAvailableDays(): Promise<JQuery> {
-	return new Cypress.Promise((resolve, reject) => {
-		cy.get(tid('consumer-calendar-day')).then((days) => {
-			resolve(days.filter('[aria-disabled=false].is-available'))
-		})
-	})
-}
-
-function findFirstPreferredTime(days:Array<HTMLElement>) {
-	return new Cypress.Promise((resolve, reject) => {
-		if (days.length === 0) {
-			reject(new Error('unable to find any available timeslots matching preferences'))
-			return
+describe('book reservation', () => {
+	
+	let patron: Patron
+	let reservation: Reservation
+	
+	before(() => {
+		patron = {
+			email: Cypress.env('email'),
+			password: Cypress.env('password'),
+			cvv: Cypress.env('cvv'),
 		}
+		cy.wrap(patron.email).should('be.ok')
+		cy.wrap(patron.password).should('be.ok')
+		cy.wrap(patron.cvv).should('be.a', 'number')
+		reservation = {
+			offering: Cypress.env('offering'),
+			partySize: Cypress.env('partySize'),
+			slug: Cypress.env('slug'),
+			timePreferences: Cypress.env('timePreferences')
+		}
+		cy.wrap(reservation.slug).should('be.ok')
+		cy.wrap(reservation.offering).should('be.ok')
+		cy.wrap(reservation.partySize).should('be.a', 'number')
+		cy.wrap(reservation.timePreferences).should('be.a', 'array')
+	})
+
+	const tid = (id:string, eq:string = '=') => `[data-testid${eq}${id}]`
+
+	function addGuestsUntilEqualTo(partySize:number) {
+		return cy
+			.get(tid('experience-dialog-content'))
+			.find(tid('guest-selector-text') + ' p')
+			.last().then((textNode) => {
+					const numberOfGuests = parseInt(textNode.text(), 10)
+					if (numberOfGuests === reservation.partySize)
+						return textNode
+					const slector = numberOfGuests > reservation.partySize 
+						? 'guest-selector_minus'
+						: 'guest-selector_plus'
+						;
+						cy.get(tid(slector)).last().click()
+						// recurse until party size is reached (or fails)
+						return addGuestsUntilEqualTo(partySize)
+			})
+	}
+
+	function closeTrusteModal() {
+		return cy
+			.get('#truste-consent-required')
+			.click() 
+	}
+
+	function openBookingModal() {
+		cy.get(tid('offering-link', '*='))
+				.contains(reservation.offering)
+				.click()
+	}
+
+	function fetchAvailableDays() {
+		return cy
+			.get(tid('consumer-calendar-day'))
+			.filter('[aria-disabled=false].is-available')
+	}
+
+	function findFirstPreferredTime(days:Array<HTMLElement>) {
+		cy.wrap(days.length).should('be.greaterThan', 0) 
 		cy.wrap(days[0]).click()
-		cy.get(tid('search-result-list-item')).then((results) => {
+		return cy.get(tid('search-result-time')).then((results) => {
 			cy.log(`searching ${results.length} available slots on ${days[0].ariaLabel} for preference...`)
 			const matchedPreferences = results
 				.filter((i, el) => 
-					target.timePreferences.indexOf(el.innerText) >= 0)
+					reservation.timePreferences.indexOf(el.innerText) >= 0)
 			if (matchedPreferences.length === 0) {
-				findFirstPreferredTime(days.slice(1)).then(resolve)
+				return findFirstPreferredTime(days.slice(1))
 			} else {
-				resolve(Cypress.$([days[0], matchedPreferences[0]]))
+				return cy.wrap(Cypress.$([days[0], matchedPreferences[0]]))
 			}
 		})
-	})
-}
+	}
 
-function authenticate() {
-	cy.visit(`https://www.exploretock.com/login?continue=%2F${targetId}`)
-	cy.get(tid('email-input')).type(patron.email)
-	cy.get(tid('email-password')).type(patron.password)
-	cy.get(tid('sign-in')).click()
-}
+	function authenticate() {
+		cy.get(tid('email-input')).type(patron.email)
+		cy.get(tid('password-input')).type(patron.password)
+		cy.get(tid('signin')).click()
+	}
 
-function checkOut(timeSlot:HTMLElement) {
-	return new Cypress.Promise((resolve, reject) => {
-		cy.wrap(timeSlot).click()
-		cy.get(tid('credit-card-form'))
-	})
-}
-
-describe(targetId, () => {
-	it('attempt booking', async () => {
-		authenticate()
+	function visit() {
+		cy.visit(`https://www.exploretock.com/login?continue=%2F${reservation.slug}`)
 		closeTrusteModal()
+	}
+
+	function fillFormFields(timeSlot:HTMLElement) {
+		cy.wrap(timeSlot).click()
+		return cy.get('body').then((body) => {
+			const root = body.find('span#cvv')
+			if (root.length) {
+				cy.intercept('https://payments.braintree-api.com/graphql').as('braintree')
+				cy.wait('@braintree')
+				return cy
+					.get('iframe[type=cvv]')
+					.its('0.contentDocument.body')
+					.find('#cvv')
+					.type(patron.cvv.toString())
+			} else {
+				return cy.wrap(null)
+			}
+		})
+	}
+
+	function submitBooking() {
+		cy.log('booking reservation...')
+		cy.get('[data-testid="submit-purchase-button"]').click()
+		cy.get('.Receipt-container--header p').then(p => {
+			cy.log(`reservation booked! ${p.text()}`)
+		})
+	}
+
+	it('for first available time preference', async () => {
+		visit()
+		authenticate()
 		openBookingModal()
-		addGuestsUntilEqualTo(target.partySize)
+		addGuestsUntilEqualTo(reservation.partySize)
 		fetchAvailableDays().then((days) => {
 			cy.log(`found ${days.length} days available to book...`)
 			return findFirstPreferredTime(Array.from(days))
 		}).then((match) => {
 			cy.log(`found match for preference: ${match[0].ariaLabel} @ ${match[1].innerText}`)
-			return checkOut(match[1])
+			return fillFormFields(match[1])
 		})
+		submitBooking()
 	})
 })
